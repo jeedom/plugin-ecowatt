2c3661d428dc814e8e0995d97c2f4d62c79e1b0e
Loïc <loic@jeedom.com>
2016-07-28 18:15:09 +0200

    Improve UI

--- desktop/php/ecowatt.php
+++ desktop/php/ecowatt.php
@@ -22,11 +22,11 @@ foreach ($eqLogics as $eqLogic) {
    </div>
 
    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-     <legend>{{Mes Eco 2 Watts}}
-     </legend>
-     <div class="eqLogicThumbnailContainer">
+       <legend>{{Mes Eco 2 Watts}}
+       </legend>
+       <div class="eqLogicThumbnailContainer">
         <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
+           <center>
             <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
         </center>
         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
@@ -46,93 +46,100 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-    <form class="form-horizontal">
-        <fieldset>
-            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
-                <div class="col-sm-3">
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement ecowatt}}"/>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                <div class="col-sm-3">
-                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                        <option value="">{{Aucun}}</option>
-                        <?php
+
+    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+    <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+    <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+        <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+    </ul>
+
+    <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+        <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+
+            <form class="form-horizontal">
+                <fieldset>
+                    <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
+                        <div class="col-sm-3">
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement ecowatt}}"/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                        <div class="col-sm-3">
+                            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                                <option value="">{{Aucun}}</option>
+                                <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                   </select>
-               </div>
-           </div>
-  <div class="form-group">
-        <label class="col-sm-3 control-label"></label>
-        <div class="col-sm-9">
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-        </div>
-    </div>
-       <div class="form-group">
-           <label class="col-sm-3 control-label">{{Type de source de données}}</label>
-           <div class="col-sm-3">
-            <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="datasource">
-                <option value="ecowatt">{{EcoWatt}}</option>
-                <!-- <option value="eco2mix">{{Eco2Mix}}</option> -->
-                <option value="ejp">{{EJP}}</option>
-                <option value="tempo">{{Tempo (EDF)}}</option>
-            </select>
-        </div>
-    </div>
-    <div class="datasource ecowatt">
-        <div class="form-group">
-            <label class="col-sm-3 control-label">{{Région}}</label>
-            <div class="col-sm-3">
-                <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="region-ew">
-                    <option value="bretagne">{{Bretagne}}</option>
-                    <option value="paca">{{Provence-Alpes-Côtes d'Azur}}</option>
-                </select>
+                           </select>
+                       </div>
+                   </div>
+                   <div class="form-group">
+                    <label class="col-sm-3 control-label"></label>
+                    <div class="col-sm-9">
+                        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                 <label class="col-sm-3 control-label">{{Type de source de données}}</label>
+                 <div class="col-sm-3">
+                    <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="datasource">
+                        <option value="ecowatt">{{EcoWatt}}</option>
+                        <!-- <option value="eco2mix">{{Eco2Mix}}</option> -->
+                        <option value="ejp">{{EJP}}</option>
+                        <option value="tempo">{{Tempo (EDF)}}</option>
+                    </select>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="datasource ejp">
-        <div class="form-group">
-            <label class="col-sm-3 control-label">{{Région}}</label>
-            <div class="col-sm-3">
-                <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="region-ejp">
-                    <option value="EJP_NORD">{{Zone Nord}}</option>
-                    <option value="EJP_OUEST">{{Zone Ouest}}</option>
-                    <option value="EJP_PACA">{{Zone Provence-Alpes-Côtes d'Azur}}</option>
-                    <option value="EJP_SUD">{{Zone Sud}}</option>
-                </select>
+            <div class="datasource ecowatt">
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">{{Région}}</label>
+                    <div class="col-sm-3">
+                        <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="region-ew">
+                            <option value="bretagne">{{Bretagne}}</option>
+                            <option value="paca">{{Provence-Alpes-Côtes d'Azur}}</option>
+                        </select>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</fieldset>
-</form>
+            <div class="datasource ejp">
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">{{Région}}</label>
+                    <div class="col-sm-3">
+                        <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="region-ejp">
+                            <option value="EJP_NORD">{{Zone Nord}}</option>
+                            <option value="EJP_OUEST">{{Zone Ouest}}</option>
+                            <option value="EJP_PACA">{{Zone Provence-Alpes-Côtes d'Azur}}</option>
+                            <option value="EJP_SUD">{{Zone Sud}}</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+    </form>
 
-<legend>{{Commandes Eco 2 Watt}}</legend>
-<table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th>{{Nom}}</th><th>{{Paramètres}}</th><th></th>
-        </tr>
-    </thead>
-    <tbody>
-    </tbody>
-</table>
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+    <table id="table_cmd" class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>{{Nom}}</th><th>{{Paramètres}}</th><th></th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 
-<form class="form-horizontal">
-    <fieldset>
-        <div class="form-actions">
-            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-        </div>
-    </fieldset>
-</form>
+</div>
+</div>
 
 </div>
 </div>
